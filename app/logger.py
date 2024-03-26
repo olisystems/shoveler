c3532e67b180c188517b17196411877fc98e05d0
m-yahya <yahya_gis@hotmail.com>
2024-03-26 13:18:01 +0500

    chore/INNODLT-347: disable logging to terminal

--- app/logger.py
+++ app/logger.py
@@ -9,14 +9,15 @@ def get_logger(name):
     file_handler = logging.FileHandler("logs/app.log")
     file_handler.setLevel(logging.DEBUG)
 
-    console_handler = logging.StreamHandler()
-    console_handler.setLevel(logging.DEBUG)
+    # console_handler = logging.StreamHandler()
+    # console_handler.setLevel(logging.DEBUG)
 
     formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+
     file_handler.setFormatter(formatter)
-    console_handler.setFormatter(formatter)
+    # console_handler.setFormatter(formatter)
 
     logger.addHandler(file_handler)
-    logger.addHandler(console_handler)
+    # logger.addHandler(console_handler)
 
     return logger
